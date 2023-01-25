1e046c6c3eb7668d0653f66ab38886a57812e963
Guudz <Kilian1702@outlook.com>
2023-01-25 03:41:18 +0100

    Initial commit

---
+++ coop.lua
@@ -0,0 +1,8 @@
+-- Editado por Nel_Br#7975 --
+-- Editado por Nel_Br#7975 --
+-- Editado por Nel_Br#7975 --
+-- Editado por Nel_Br#7975 --
+-- Editado por Nel_Br#7975 --
+
+-- Discord:https://discord.gg/qATpMnP
+-- Script e Key Patreon
---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+game 'gta5'
+-- Editado por Nel_Br#7975 --
+-- Editado por Nel_Br#7975 --
+-- Editado por Nel_Br#7975 --
+-- Editado por Nel_Br#7975 --
+-- Editado por Nel_Br#7975 --
+
+client_scripts {
+	"@vrp/lib/utils.lua",
+	"menu.lua",
+	"lscustoms.lua",
+	"lsconfig.lua"
+}
+
+server_scripts {
+	"@vrp/lib/utils.lua",
+	"lscustoms_server.lua"
+}
